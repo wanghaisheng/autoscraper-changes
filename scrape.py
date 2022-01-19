3c7cd29d0cbd207e2ef96d6a4f0b8d4a3cb865a3
wanghaisheng <edwin_uestc@163.com>
2022-01-20 05:43:40 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,36 +1,56 @@
 import requests
+from amazon_buddy import Product, AmazonBuddy, Category, SortType
 
 import pandas as pd
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.bbc.com/")
-doc = BeautifulSoup(response.text, 'html.parser')
 
-stories = doc.select('.media-list__item')
-
+ab = AmazonBuddy(debug=True)
+df_queries = pd.read_csv("queries.csv")
+queries = df_queries.queries
 rows = []
 
-for story in stories:
-    row = {}
+for query in queries:
 
-    row['title'] = story.select_one('h3').text.strip()
+    products = ab.search_products(
+        query,
+        sort_type=SortType.PRICE_HIGH_TO_LOW,
+        min_price=0,
+        category=Category.BEAUTY_AND_PERSONAL_CARE,
+        max_results=500
+    )
+    for p in products:
+        row={}
+        row['asin']=p.asin
+        rows.append(p)
+        reviews = ab.get_reviews(asin=p.asin)
+        print(reviews)
+        reviews.save('{}-reviews.json'.format(p.asin))
 
-    try:
-        row['href'] = story.select_one('.media__link, .reel__link')['href']
-    except:
-        pass
+        ab.get_product_details(p.asin).save('{}.json'.format(p.asin))
+# ab.get_product_details(asin).jsonprint()
 
-    try:
-        row['tag'] = story.select_one('.media__tag').text.strip()
-    except:
-        pass
+# for story in stories:
+#     row = {}
 
-    try:
-        row['summary'] = story.select_one('.media__summary').text.strip()
-    except:
-        pass
+#     row['title'] = story.select_one('h3').text.strip()
 
-    rows.append(row)
+#     try:
+#         row['href'] = story.select_one('.media__link, .reel__link')['href']
+#     except:
+#         pass
+
+#     try:
+#         row['tag'] = story.select_one('.media__tag').text.strip()
+#     except:
+#         pass
+
+#     try:
+#         row['summary'] = story.select_one('.media__summary').text.strip()
+#     except:
+#         pass
+
+#     rows.append(row)
 
 df = pd.DataFrame(rows)
 df.to_csv("bbc-headlines.csv", index=False)
