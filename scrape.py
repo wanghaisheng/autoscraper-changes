ee211a502a20ad8a1fb112d005b2e19c09bf0838
wanghaisheng <edwin_uestc@163.com>
2022-01-20 05:56:31 +0800

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,33 +1,33 @@
 import requests
 from amazon_buddy import Product, AmazonBuddy, Category, SortType
-
+from pathlib import Path
 import pandas as pd
 from bs4 import BeautifulSoup
 
-
+OUTPUT_DIR = Path("data")
 ab = AmazonBuddy(debug=True)
 df_queries = pd.read_csv("queries.csv")
 queries = df_queries.queries
 rows = []
-
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 for query in queries:
 
     products = ab.search_products(
         query,
         sort_type=SortType.PRICE_HIGH_TO_LOW,
         min_price=0,
-        category=Category.BEAUTY_AND_PERSONAL_CARE,
+        # category=Category.BEAUTY_AND_PERSONAL_CARE,
         max_results=500
     )
     for p in products:
-        row={}
-        row['asin']=p.asin
-        rows.append(p)
-        reviews = ab.get_reviews(asin=p.asin)
-        print(reviews)
-        reviews.save('{}-reviews.json'.format(p.asin))
+        # row={}
+        # row['asin']=p.asin
+        # rows.append(p)
+        # reviews = ab.get_reviews(asin=p.asin)
+        # print(reviews)
+        ab.get_reviews(asin=p.asin).save(OUTPUT_DIR+'/'+'{}-reviews.json'.format(p.asin))
 
-        ab.get_product_details(p.asin).save('{}.json'.format(p.asin))
+        ab.get_product_details(p.asin).save(OUTPUT_DIR+'/'+'{}.json'.format(p.asin))
 # ab.get_product_details(asin).jsonprint()
 
 # for story in stories:
@@ -52,5 +52,5 @@ for query in queries:
 
 #     rows.append(row)
 
-df = pd.DataFrame(rows)
-df.to_csv("bbc-headlines.csv", index=False)
+# df = pd.DataFrame(rows)
+# df.to_csv("bbc-headlines.csv", index=False)
