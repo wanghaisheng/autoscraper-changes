7b47626e85e8ea52e444a17054305cff1c6e3a85
wanghaisheng <edwin_uestc@163.com>
2022-01-24 21:19:32 +0800

    Update reviews.py

--- reviews.py
+++ reviews.py
@@ -29,7 +29,7 @@ def play_store_scraper(package):
 applerows = []
 
 def app_store_scraper(app_name):
-    app = AppStore(app_name=app_name)
+    app = AppStore(country="us",app_name=app_name)
     app.review(how_many=7000)
 #     app.review()
 
