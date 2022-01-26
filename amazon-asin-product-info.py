207b0e6437311adcb53c4eac9d137ca117b85650
wanghaisheng <edwin_uestc@163.com>
2022-01-26 22:01:37 +0800

    Create amazon-asin-product-info.py

---
+++ amazon-asin-product-info.py
@@ -0,0 +1,26 @@
+from amazon_buddy import Product, AmazonBuddy, Category, SortType
+import pandas as pd
+
+
+OUTPUT_DIR = Path("data")
+
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+OUTPUT_DIR = str(OUTPUT_DIR)
+
+ab = AmazonBuddy(debug=True, user_agent='ADD_USER_AGENT')
+
+
+df_queries = pd.read_csv("asin-queries-unique.csv")
+
+
+
+for i, query in df_queries.iterrows():
+    # print(query)
+    asin=query['asin']
+    print(asin)
+    reviews = ab.get_reviews(asin=asin)
+    # print(reviews)
+    reviews.save(OUTPUT_DIR+'{}-reviews.json'.format(asin))
+
+    ab.get_product_details(asin).save(OUTPUT_DIR+'{}.json'.format(asin))
+
