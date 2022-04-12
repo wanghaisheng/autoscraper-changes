64829961f7b3e5aca1f9b421cfc8e6567932b20b
wanghaisheng <edwin.uestc@gmail.com>
2022-04-12 11:33:04 +0800

    fix

--- utils.py
+++ utils.py
@@ -28,6 +28,14 @@ import time
 from pyairtable.formulas import match
 from pyairtable import *
 from playwright.async_api import async_playwright, Browser
+
+from enum import Enum
+
+
+class BrowsersEnum(Enum):
+    FIREFOX = 1
+    CHROME = 2
+    CHROMIUM = 3
 HEADERS = {
     'User-Agent':
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36',
@@ -562,7 +570,7 @@ def get_undetected_webdriver():
     # Use with Chromium
     # Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
     # Use with Chrome
-    Service(ChromeDriverManager().install())
+    
     driver = ''
     if os.environ.get("CHROMEDRIVER_PATH"):
         executable_path = os.environ.get("CHROMEDRIVER_PATH")
@@ -581,6 +589,36 @@ def get_undetected_webdriver():
         else:
             print('start uc with proxy')
 
-            driver = webdriver.Chrome(
+            driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
             options=options, seleniumwire_options=seleniumwire_options)
     return driver
+
+
+def get_a_driver(browser: BrowsersEnum, is_headless=True, seleniumwire_options=None):
+    if browser is BrowsersEnum.FIREFOX:
+        options = webdriver.FirefoxOptions()
+        options.headless = is_headless
+        from selenium.webdriver.firefox.service import Service
+        from webdriver_manager.firefox import GeckoDriverManager
+        service = Service(GeckoDriverManager().install())
+        return webdriver.Firefox(service=service, options=options, seleniumwire_options=seleniumwire_options)
+    if browser is BrowsersEnum.CHROME:
+        options = webdriver.ChromeOptions()
+        options.headless = is_headless
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.chrome import DriverManager
+        service = Service(DriverManager().install())
+        return webdriver.Chrome(service=service, options=options)
+    if browser is BrowsersEnum.CHROMIUM:
+        from selenium.webdriver.chromium.options import ChromiumOptions
+        from selenium.webdriver.chromium.webdriver import ChromiumDriver
+        from selenium.webdriver.chromium.webdriver import ChromiumDriver
+        options = ChromiumOptions()
+        options.headless = is_headless
+        from selenium.webdriver.chromium.service import ChromiumService
+        from webdriver_manager.chrome import ChromeDriverManager
+        from webdriver_manager.utils import ChromeType
+        service = ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(),
+                                  start_error_message='Couldnt start')
+        return ChromiumDriver(service=service, options=options, browser_name="chromium", vendor_prefix="webkit",
+                              seleniumwire_options=seleniumwire_options)
