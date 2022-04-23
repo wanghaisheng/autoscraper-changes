d81fe69a09b6b9e4e13b6c42c62091cd2c650d8b
wanghaisheng <edwin.uestc@gmail.com>
2022-04-23 11:01:00 +0800

    adjust to scroll

--- idata.py
+++ idata.py
@@ -91,19 +91,19 @@ async def get_playright(proxy: bool = False, headless: bool = True):
 #         print()
 
 
-async def scrape_pl(search_query="python", topic='upwork',db='CFLS'):
+async def scrape_pl(search_query="python", topic='upwork', db='CFLS'):
     # time.sleep(random.randint(10,50))
     # Sends a request
-    print('=============',search_query,topic,db)
-    shuobo=False
-    if db=='CDMD':
-        shuobo =True
+    print('=============', search_query, topic, db)
+    shuobo = False
+    if db == 'CDMD':
+        shuobo = True
     start = time.time()
 
-    loginurl='https://user.cn-ki.net/login'
+    loginurl = 'https://user.cn-ki.net/login'
 
     url = "https://search.cn-ki.net/search?keyword={}&db={}".format(
-        search_query,db)
+        search_query, db)
     print('user home url', url)
     browser = await get_playright(False, False)
     context = await browser.new_context()
@@ -123,219 +123,132 @@ async def scrape_pl(search_query="python", topic='upwork',db='CFLS'):
         res = await homepage.goto(loginurl)
         ua = os.environ.get('ua')
         us = os.environ.get('us')
-        
 
         await homepage.fill('#num', ua)
-        #passwd
+        # passwd
         await homepage.fill('#passwd', us)
         await homepage.locator('#app > div > div > div:nth-child(3) > div > div > div.card__title.card__title--primary > div:nth-child(3) > button > div').click()
-        time.sleep(3)
-        res = await homepage.goto('https://search.cn-ki.net/search?keyword=%E8%84%91%E7%98%AB%20%E8%90%A5%E5%85%BB&db=CDMD&p=-1')
+        res = await homepage.goto('https://search.cn-ki.net/advance_search')
         # print(parser.find(string=re.compile("found")))
         # print(parser.find_all(attrs={"data-test": "jobs-count"}))
         avatar = homepage.locator(
             'span.mdui-float-right:nth-child(4) > a:nth-child(1)')
         print('is login in')
-        
-        await homepage.fill('#keyword',search_query)
+
+        await homepage.fill('#keyword', search_query)
         print('input keyword')
-        await homepage.locator('.mdui-col-xs-2').click()
+        time.sleep(1)
+
+        await homepage.locator('div.mdui-col-xs-2:nth-child(3)').click()
         print('action to search')
+        time.sleep(1)
 
-        count=0
-        try:
-        
-            count = homepage.locator('.pagerTitleCell')
-            count = await count.text_content()
-            print('count', count)
-            count = count.strip().replace('找到', '').replace('条结果', '')
-        except:
+        await homepage.locator('div.mdui-col-xs-10:nth-child(1) > span:nth-child(2)').click()
+        # count = 0
+        # try:
 
-            error = homepage.locator('.TitleLeftCell')
-            error_msg = await error.text_content()
-            print('error_msg',error_msg)
-            error_flag=True
-            while error_flag:
-                if '系统' in error_msg:
-                    await homepage.reload()
-                    await homepage.locator('.mdui-btn-dense').click()
-                    time.sleep(6)
-                    print('detected error refresh')
-                    print('retry count no')
-                    count = homepage.locator('.pagerTitleCell')
-                    count = await count.text_content()
-                    print('count', count)
-                    count = count.strip().replace('找到', '').replace('条结果', '')
-                    if count:
-                        error_flag=False
-                        break
-                else:
-                    print('no error at all')
+        #     count = homepage.locator('.pagerTitleCell')
+        #     count = await count.text_content()
+        #     print('count', count)
+        #     count = count.strip().replace('找到', '').replace('条结果', '')
+        # except:
 
-                    error_flag=False
-                    break
+        #     error = homepage.locator('.TitleLeftCell')
+        #     error_msg = await error.text_content()
+        #     print('error_msg', error_msg)
+        #     error_flag = True
+        #     while error_flag:
+        #         if '系统' in error_msg:
+        #             await homepage.reload()
+        #             await homepage.locator('.mdui-btn-dense').click()
+        #             time.sleep(6)
+        #             print('detected error refresh')
+        #             print('retry count no')
+        #             count = homepage.locator('.pagerTitleCell')
+        #             count = await count.text_content()
+        #             print('count', count)
+        #             count = count.strip().replace('找到', '').replace('条结果', '')
+        #             if count:
+        #                 error_flag = False
+        #                 break
+        #         else:
+        #             print('no error at all')
 
-            print('retry count no')
-            count = homepage.locator('.pagerTitleCell')
-            count = await count.text_content()
-            print('count', count)
-            count = count.strip().replace('找到', '').replace('条结果', '')
+        #             error_flag = False
+        #             break
+
+        #     print('retry count no')
+        if db=='CDMD':
+            print('choose shuobo')
+            await homepage.locator('label.mdui-radio:nth-child(3)').click()         
+        count = homepage.locator('div.mdui-col-xs-2:nth-child(2)')
+        count = await count.text_content()
+        print('total papers', count)
+        count = count.strip().replace('找到', '').replace('条结果', '')
 
         if ',' in count:
             count = count.replace(',', '')
-        print(count)
         count = int(count)
-        pages = int(count/20)+1
+        pages = 1
+        if count > 20:
+            pages = int(count/20)+1
         result = []
-        print(pages)
+        print('need click times',pages)
+           
         for p in range(pages):
-            print(p, '---')
-            prefix = 'search?keyword={}&db={}'.format(
-                search_query,db)
-            print('prefix', prefix)
-        # 3089
-        # https://www.upwork.com/nx/jobs/search/?q=tiktok&sort=recency
-        # 550
-            url = prefix+'&p='+str(p+1)
-            print('deal page', p+1, url)
-            # fenyepage = await context.new_page()
-            fenyepage =homepage
-            # 排序
-            await fenyepage.locator('.mdui-color-pink > small:nth-child(1)').click()
-
-            if p>0:
-                pagi=fenyepage.locator('a[href={}]'.format(url))
-                print('pagi,',pagi)
-                await pagi.click()
-
-            try:
+            jobs = homepage.locator('#result_main > div.mdui-row')
             
-                count = fenyepage.locator('.pagerTitleCell')
-                count = await count.text_content()
-                print('count', count)
-                count = count.strip().replace('找到', '').replace('条结果', '')
-            except:
-
-                error = fenyepage.locator('.TitleLeftCell')
-                error_msg = await error.text_content()
-                print('error_msg',error_msg)
-                error_flag=True
-                while error_flag:
-                    if '系统' in error_msg:
-
-                        print('detected error refresh')
-                        print('retry count no')
-                        await fenyepage.goto(url)
-                        # await fenyepage.fill('#keyword',search_query)
-                        # print('type input',search_query)
-                        # await fenyepage.locator('.mdui-col-xs-2').click()
-                        time.sleep(6)
-                        count = fenyepage.locator('.pagerTitleCell')
-                        count = await count.text_content()
-                        print('count', count)
-                        count = count.strip().replace('找到', '').replace('条结果', '')
-                        if count:
-                            error_flag=False
-                            break
-                    else:
-                        print('no error at all')
-
-                        error_flag=False
-                        break
-            jobs = fenyepage.locator('div.mdui-container:nth-child(2) > div:nth-child(3) > div:nth-child(1) > h3:nth-child(1) > a')
-            print('!!',await jobs.count())
-            jobs = fenyepage.locator('a[href^="/doc_detail"]')
             jobcount = await jobs.count()
-            print('papercount', jobcount)
             await asyncio.sleep(1)
-            if jobcount > 0:
-                for i in range(0, jobcount):
-                    # time.sleep(random.randint(10, 30))
-                    print('no ', i, ' of total paper ',
-                            jobcount, 'in this page', p)
-                    # title= jobs.nth(i).locator("div > h3 >a")
-                    # title=await title.text_content()
-                    # print('title',title)
-                    # href= jobs.nth(i).locator('a[href^="/shop/url/"]')
-                    # print(href)
-                    href = await jobs.nth(i).get_attribute('href')
-                    print('href', href)
+            print('this pagi',jobcount)
+            # jobs = jobs[0+p*20:0+p*19+19]
+            for i in range(0+p*20, p*19+19):
+                print('dealing index',i,'paper')
+                url = await jobs.nth(i).locator('div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').get_attribute('href')
+                url = 'https://search.cn-ki.net'+url
+                print('url',url)
+                title = await jobs.nth(i).locator('div:nth-child(1) > h3:nth-child(1) > a:nth-child(1)').text_content()
+                print('title',title)
 
-                    url = 'https://search.cn-ki.net'+href
-                    data = supabase.table("papers_bianmi").select('url').eq('url',url).execute()
-                    if len(data.data) > 0:
-                        print('this url exist', url, data.data)
-                    else:
-                        jobpage = await context.new_page()
+                raw = await jobs.nth(i).locator('div:nth-child(1) > div:nth-child(2)').text_content()
+                raw=raw.strip().split('，')
+                raw = list(filter(None, raw))
 
-                        await jobpage.goto(url)
-                        title = jobpage.locator('.headline')
-                        title = await title.text_content()
-                        print('title', title)
-                        await asyncio.sleep(3)
-                        container = jobpage.locator('div.card >div.container >div.layout')
-                        journal=''
-                        institute=''
-                        journal_time=''
-                        keywords=''
-                        author=''
-                        print(await container.count(),'jibnexinxi ')
-                        for i in range(0, await container.count()):
-                            alltext= await container.nth(i).text_content()
-                            alltext=alltext.strip().replace('\r', '').replace('\n', '')
-                            alltext=alltext.replace(' ','')
-                            print('jibenxinx--',alltext)
-                            if '单位' in alltext:
-                                institute = alltext.replace('单位','').strip()
-                            elif '期刊' in alltext:
-                                journal = alltext.replace('期刊','').strip()
-                            elif '时间' in alltext:
-                                journal_time= alltext.replace('时间','').strip()
-                            elif '关键词' in alltext:
-                                keywords= alltext.replace('关键词','').strip()
-                            elif '作者' in alltext:
-                                author= alltext.replace('作者','').strip()
-                                                            
-                        abstract = jobpage.locator('.card__text')
-                        abstract = await abstract.text_content()
-                        abstract = abstract.strip().replace('\r', '').replace('\n', '')
-                        print('abstract',abstract)
+                author = raw[0].strip()
+                # print('raw',raw[1])
 
+                print('author',author)
+                journal = raw[1].strip().split(' ')[0]
+                print('journal',journal)
 
-                        async with jobpage.expect_download() as download_info:
-                            # Perform the action that initiates download
-                            # await jobpage.click("a.success--text")
-                            await jobpage.click('text=下载')
-                        download = await download_info.value
-                        print('downloadinfo',download)
+                journal_time= raw[1].strip().split(' ')[1]
+                print('journal_time',journal_time)
 
-                        # Wait for the download process to complete
-                        path = await download.path()
-                        print('downloadpath',path)
+                # journal_time= raw[1].strip().split(' ')[1]
 
-                        filename=download.suggested_filename
-                        print(filename)
+                abstract = await jobs.nth(i).locator('div:nth-child(1) > div:nth-child(3)').text_content()
+                pdflink = await jobs.nth(i).locator('div:nth-child(1) > div:nth-child(4) > a:nth-child(1)').get_attribute('href')
+                print('pdflink',pdflink)
 
-                        await download.save_as('./data/bianmi/'+filename)
-            # Making a list of dicts
-                        paper = {
-                            "url": url,
-                            "title": title,
-                            "author": author,
-                            "keywords": keywords,
-                            "shuobo":shuobo,
-                            "abstract": abstract,
-                            "institute": institute,
-                            "journal":journal,
-                            "journal_time":journal_time,
-                            "topic": topic
-                        }
-                        print('save db')
-                        data = supabase.table("papers_bianmi").insert(paper).execute()
-                        print('delay for later')
-                        # await asyncio.sleep(3)
-                        time.sleep(30)
-                    # jobpage.close()
+                paperbrief = {
+                    "url": url.strip(),
+                    "title": title,
+                    "author": author,
+                    "shuobo": shuobo,
+                    "abstract": abstract,
+                    "journal": journal,
+                    "journal_time": journal_time,
+                    "pdflink":pdflink,
+                    "topic": topic
+                }
+                print('save db')
+                data = supabase.table("papers_bianmi").insert(
+                    paperbrief).execute()
+                print('delay for later')
+
+                # jobpage.close()
+            await homepage.locator('.mdui-m-b-5').click()
+            time.sleep(5)                
 # create table papers_bianmi (
 #   id bigint generated by default as identity primary key,
 #   inserted_at timestamp with time zone default timezone('utc'::text, now()) not null,
@@ -351,6 +264,83 @@ async def scrape_pl(search_query="python", topic='upwork',db='CFLS'):
 
     except:
         print('error')
+
+
+async def downloadpdf(url,context,shuobo):
+    # url = 'https://search.cn-ki.net'+href
+    data = supabase.table("papers_bianmi").select(
+        'url').eq('url', url.strip()).execute()
+    if len(data.data) > 0:
+        print('this url exist', url, data.data)
+    else:
+        jobpage = await context.new_page()
+
+        await jobpage.goto(url)
+        title = jobpage.locator('.headline')
+        title = await title.text_content()
+        print('title', title)
+        await asyncio.sleep(3)
+        container = jobpage.locator('div.card >div.container >div.layout')
+        journal = ''
+        institute = ''
+        journal_time = ''
+        keywords = ''
+        author = ''
+        print(await container.count(), 'jibnexinxi ')
+        for i in range(0, await container.count()):
+            alltext = await container.nth(i).text_content()
+            alltext = alltext.strip().replace('\r', '').replace('\n', '')
+            alltext = alltext.replace(' ', '')
+            print('jibenxinx--', alltext)
+            if '单位' in alltext:
+                institute = alltext.replace('单位', '').strip()
+            elif '期刊' in alltext:
+                journal = alltext.replace('期刊', '').strip()
+            elif '时间' in alltext:
+                journal_time = alltext.replace('时间', '').strip()
+            elif '关键词' in alltext:
+                keywords = alltext.replace('关键词', '').strip()
+            elif '作者' in alltext:
+                author = alltext.replace('作者', '').strip()
+
+        abstract = jobpage.locator('.card__text')
+        abstract = await abstract.text_content()
+        abstract = abstract.strip().replace('\r', '').replace('\n', '')
+        # print('abstract',abstract)
+
+        async with jobpage.expect_download() as download_info:
+            # Perform the action that initiates download
+            # await jobpage.click("a.success--text")
+            await jobpage.click('text=下载')
+        download = await download_info.value
+        print('downloadinfo', download)
+
+        # Wait for the download process to complete
+        path = await download.path()
+        print('downloadpath', path)
+
+        filename = download.suggested_filename
+        print(filename)
+
+        await download.save_as('./data/bianmi/'+filename)
+        # Making a list of dicts
+        paper = {
+            "url": url.strip(),
+            "title": title,
+            "author": author,
+            "keywords": keywords,
+            "shuobo": shuobo,
+            "abstract": abstract,
+            "institute": institute,
+            "journal": journal,
+            "journal_time": journal_time,
+            "topic": topic
+        }
+        print('save db')
+        data = supabase.table("papers_bianmi").insert(paper).execute()
+        print('delay for later')
+        # await asyncio.sleep(3)
+        time.sleep(30)
 
 
 def ensure_dir(file):
@@ -409,7 +399,7 @@ topic = opts.topic
 keywords = '便秘'
 # asyncio.run(scrape_data(search_query=keywords, topic=topic))
 
-asyncio.run(scrape_pl(search_query=keywords, topic=topic,db='CDMD'))
+asyncio.run(scrape_pl(search_query=keywords, topic=topic, db='CDMD'))
 
 # asyncio.run(scrape_pl(search_query=keywords, topic=topic,db='CFLS'))
 
